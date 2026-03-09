d62d0e9827ba7df55aa488f7245d45555d3b3935
David Miculit <David.Miculit@cognizant.com>
2026-03-09 14:14:49 +0200

    feat: add sqalchemy

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from botbox!")
+import uvicorn
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+def main():
+    uvicorn.run("main:app", host='0.0.0.0', port = 8000, reload=True)
 
 if __name__ == "__main__":
     main()
